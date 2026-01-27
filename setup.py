fc8f8cf50333b60df19b4c102f415ebad632876b
DhaniAAA <ramadhanigb19@gmail.com>
2026-01-27 13:24:01 +0700

    Fix: Remove redundant License classifier to comply with PEP 639

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
